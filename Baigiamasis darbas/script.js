82c8b673cf1cfbf5bba62b30a735a3ba4fa59718
Patre <svencionytepatricija5@gmail.com>
2025-12-03 14:33:52 +0200

    removed js

--- Baigiamasis darbas/script.js
+++ Baigiamasis darbas/script.js
@@ -1,16 +1,1 @@
-class INFO {
-    constructor(name, income, housing, food, transport, other, debt, savings){
-        this.name = 
-    }
-}
 
-
-const fs = require('fs');
-const filePath = 'finance-data.txt';
-const content = fs.readFileSync(filePath, 'utf-8');
-const lines = content.split(/\r?\n/);
-
-// Cia kaip sukant for cikla filo 
-for(const line of lines){
-    console.log(line);
-}
